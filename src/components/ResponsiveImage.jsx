2221dba4252b369d46ecac879f364ccdccd93b29
kyouichitani-cloud <kyouichitani@icloud.com>
2026-08-15 02:10:51 +0900

    モバイルSpeed Indexを最適化

--- src/components/ResponsiveImage.jsx
+++ src/components/ResponsiveImage.jsx
@@ -1,8 +1,9 @@
 export default function ResponsiveImage({ src, alt, ...props }) {
   const filename = src.split('/').pop()
+  const fetchPriority = props.fetchPriority ?? (props.loading === 'lazy' ? 'low' : undefined)
 
   return <picture className="responsive-picture">
     <source media="(max-width: 767px)" srcSet={`images/mobile/${filename}`}/>
-    <img src={src} alt={alt} {...props}/>
+    <img src={src} alt={alt} {...props} fetchPriority={fetchPriority}/>
   </picture>
 }
